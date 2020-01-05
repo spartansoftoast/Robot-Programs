657958829ff4550e07bee3b76420996f2a920456
spartansoftoast <56944298+spartansoftoast@users.noreply.github.com>
2020-01-05 15:05:10 -0500

    Delete testing.py

--- Active Missions/testing.py
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env pybricks-micropython
-import functions as toast # import the functions we created to help mission programmers
-import Missions as missions # import the mission functions each team member programmed
-
-#toast.Bridge()
-#toast.followLeft(10)
-#toast.moveWhite()
-toast.med_attachment(-50)
